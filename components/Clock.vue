ab6a54953f2e92468b6e226a850171fa4da4c31f
SIeep <brandon_n_evans@live.com>
2019-09-06 13:42:37 -0500

    init commit WIP

---
+++ components/Clock.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        {{datenow}}
+    </div>
+</template>
+
+<script>
+export default {
+  components: {
+    
+  },
+  created() {
+    
+  },
+  data() {
+    return {
+      datenow: ''
+    }
+  },
+  methods: {
+    time() {
+      this.datenow = moment().format('h:mm:ss a')
+
+    },
+  },
+  mounted() {
+    this.interval = setInterval(this.time, 1000)
+  },
+   beforeDestroy() {
+    clearInterval(this.interval)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/Quotes.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <section>
+            {{quotes}}
+        </section>  
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            quotes: [
+                {id: 1, text: 'Well, we\'re all just dying anyways.'},
+                {id: 2, text: 'It\'s ok to be sad.'}
+            ]
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
